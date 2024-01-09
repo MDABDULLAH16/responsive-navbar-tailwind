4788fdb9be04c852a8cc051c8da0eb4377f5b5e6
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-09 13:52:36 +0600

    btn toggle

--- src/Link/Link.jsx
+++ src/Link/Link.jsx
@@ -4,7 +4,7 @@ const Link = ({ route }) => {
   const { name, path } = route;
   return (
     <div>
-      <li className="ml-4 font-medium ">
+      <li className="ml-4 font-medium hover:bg-orange-400 pr-2">
         <a href={path}>{name}</a>
       </li>
     </div>
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "../Link/Link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     {
       id: 1,
@@ -30,8 +32,22 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav>
-      <div className=" list-none md:flex  bg-indigo-600 text-white w-100 ">
+    <nav className="bg-indigo-400">
+      <div onClick={() => setOpen(!open)} className="md:hidden ">
+        <span>
+          {open === true ? (
+            <XMarkIcon className="h-6 w-6 "></XMarkIcon>
+          ) : (
+            <Bars3Icon className="h-6 w-6"></Bars3Icon>
+          )}
+        </span>
+      </div>
+
+      <div
+        className={` list-none md:flex md:static absolute duration-500 bg-indigo-400 text-white  ${
+          open ? "top-6" : "-top-36"
+        }`}
+      >
         {routes.map((route) => (
           <Link route={route}></Link>
         ))}
